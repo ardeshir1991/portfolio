b081004b96f2af4c5fe1b2189007b8c71c5fda1b
ardeshir <ardeshiref1991@gmail.com>
2022-08-12 13:15:49 +0430

    contact us page

---
+++ pages/contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from "../components/contact/Contact";
+import Layout from "../components/Layout";
+
+const contact = () => {
+  return (
+    <Layout>
+      <Contact />
+    </Layout>
+  );
+};
+
+export default contact;
